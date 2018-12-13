2f13f50d981158130ae99f6e8fab395f4a06546a
zouguotao <2506526350@qq.com>
2018-12-13 10:18:30 +0800

    add public/products to linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :sidekiq_config, "config/sidekiq.yml"
 # append :linked_files, "config/database.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "storage", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "storage", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/products"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
